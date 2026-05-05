1c6367fc9ded7d85063c4d6a05139d850ef703d0
xiaokalami1299 <2791132103@qq.com>
2026-05-05 18:57:39 +0800

    Delete resume directory

--- resume/src/main/java/com/example/resume/service/ResumeAnalysisService.java
+++
@@ -1,9 +0,0 @@
-package com.example.resume.service;
-
-import com.example.resume.dto.ResumeAnalysisRequest;
-import com.example.resume.dto.ResumeAnalysisResponse;
-
-public interface ResumeAnalysisService {
-
-    ResumeAnalysisResponse analyzeResume(ResumeAnalysisRequest request);
-}
